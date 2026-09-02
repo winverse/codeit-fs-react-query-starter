6dc44cc61e3253dcf515e6fc968c288a6b9f7e63
winverse <public.winverse@gmail.com>
2026-09-02 21:39:02 +0900

    docs(topic-28): 업로드 TODO를 기능 중심으로 정리한다

--- src/domains/feed/PostUploader/PostUploader.jsx
+++ src/domains/feed/PostUploader/PostUploader.jsx
@@ -4,9 +4,9 @@ import { PostForm } from '@/domains/feed/PostForm';
 import { useLoginContext } from '@/contexts/LoginContext';
 
 /*
-TODO(3-01): 업로드 로직을 뮤테이션으로 구현합니다.
-- uploadPost 호출
-- mutate 연결 및 isPending으로 버튼 제어
+TODO(3-01): 포스트 업로드 요청을 연결합니다.
+- 입력 폼이 전달한 포스트를 서버로 보냅니다.
+- 요청 중에는 업로드 버튼을 비활성화합니다.
 
 TODO(3-03): 업로드 성공 후 화면을 동기화합니다.
 - 업로드한 포스트가 반영되는 전체 목록과 사용자별 목록 갱신
